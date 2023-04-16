6c595951ef43bfbb410528e645a6ff07c6abfd32
A-dead-pixel <me@otsrv.net>
2023-04-16 15:37:42 +0200

    Update dependencies
    This seems to resolve an esoteric bug causing db corruption.

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ setup(
     install_requires=[
         'bsddb3==6.2.7',
         'flup6',
-        'gunicorn==19.9.0',
-        'gevent==1.3.1',
-        'greenlet==0.4.13',  # for compatibility with this version of gevent
+        'gunicorn<20.2',
+        'gevent<22.11',
+        'greenlet<2.1',
         'progressbar2',
         'requests',
         'six',
